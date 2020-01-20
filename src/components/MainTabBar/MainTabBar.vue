bd52b32c206d478418fea5a4e90fd7c1e327e30f
Formerscholar <2319315514@qq.com>
2020-01-20 20:28:56 +0800

    a vue test update

--- src/components/MainTabBar/MainTabBar.vue
+++ src/components/MainTabBar/MainTabBar.vue
@@ -1,37 +1,37 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="#e93b3d">
-      <img slot="item-icon" src="../../assets/img/tabbar/category.png" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/category-active.png" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/category-active.png" alt />
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="#e93b3d">
-      <img slot="item-icon" src="../../assets/img/tabbar/category.png" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/category-active.png" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/category-active.png" alt />
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/cart" activeColor="#e93b3d">
-      <img slot="item-icon" src="../../assets/img/tabbar/category.png" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/category-active.png" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/category-active.png" alt />
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="/profile" activeColor="#e93b3d">
-      <img slot="item-icon" src="../../assets/img/tabbar/category.png" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/category-active.png" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/category-active.png" alt />
       <div slot="item-text">我的</div>
     </tab-bar-item>
   </tab-bar>
 </template>
 <script>
-  import TabBar from '../tabbar/TabBar'
-  import TabBarItem from '../tabbar/TabBarItem'
-  export default {
-    name: "MainTabBar",
-    components: {
-      TabBar,
-      TabBarItem
-    }
-  };
+import TabBar from "components/tabbar/TabBar";
+import TabBarItem from "components/tabbar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
 </script>
 <style>
 </style>
